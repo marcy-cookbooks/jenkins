4bc97cc96b7e8464cd617bb6ab4437fdecccfb5c
Marcy <marcy9114@gmail.com>
2014-04-04 15:29:34 +0900

    add test

---
+++ test/integration/default/serverspec/jenkins_spec.rb
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+
+require 'serverspec'
+require 'pathname'
+
+include Serverspec::Helper::Exec
+include Serverspec::Helper::DetectOS
+
+RSpec.configure do |c|
+  c.before :all do
+    c.os = backend(Serverspec::Commands::Base).check_os
+    c.path = '/sbin:/usr/bin'
+  end
+end
+
+describe package('jenkins') do
+  it { should be_installed }
+end
+
+describe service('jenkins') do
+  it { should be_enabled }
+  it { should be_running }
+end
